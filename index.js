8907ffb0b6eca490804dda7b223aef0a182ece25
Mario Repas <mario.repas63@gmail.com>
2023-02-11 19:40:24 -0500

    added loop to generate a new file rather than overwrite

--- index.js
+++ index.js
@@ -47,20 +47,26 @@ inquirer.prompt([
 .then((response) => {
 
     const shape = new Shape(response.text, response.textColor, response.backgroundColor);
+// Loop to check if file exists and create new file name if it does.
+    let filename = 'examples/logo.svg';
+    let i = 0;
+    while (fs.existsSync(filename)) {
+    filename = `examples/logo-${i++}.svg`;
+    }
 
     if (response.shape === 'Triangle') {
         const triangle = new Triangle(response.text, response.textColor, response.backgroundColor);
-        fs.writeFile('examples/logo.svg', triangle.render(), (err) =>
+        fs.writeFile(filename, triangle.render(), (err) =>
             err ? console.log(err) : console.log('Success!')
         );
     } else if (response.shape === 'Square') {
         const square = new Square(response.text, response.textColor, response.backgroundColor);
-        fs.writeFile('examples/logo.svg', square.render(), (err) =>
+        fs.writeFile(filename, square.render(), (err) =>
             err ? console.log(err) : console.log('Success!')
         );
     } else if (response.shape === 'Circle') {
         const circle = new Circle(response.text, response.textColor, response.backgroundColor);
-        fs.writeFile('examples/logo.svg', circle.render(), (err) =>
+        fs.writeFile(filename, circle.render(), (err) =>
             err ? console.log(err) : console.log('Success!')
         );
     }
